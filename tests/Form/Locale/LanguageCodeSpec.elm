ba4346cf590f24140764eb8bea5e58f195fbe49b
Dominic Endrei <dominic.endrei@gmail.com>
2022-05-06 13:12:03 +1000

    Typo

--- tests/Form/Locale/LanguageCodeSpec.elm
+++ tests/Form/Locale/LanguageCodeSpec.elm
@@ -7,7 +7,7 @@ import Test exposing (..)
 
 suite : Test
 suite =
-    describe "Forms.Local.LanguageCode roundtrip"
+    describe "Forms.Locale.LanguageCode round trip"
         ([ LanguageCode.AA, LanguageCode.AB, LanguageCode.AF, LanguageCode.AK, LanguageCode.AM, LanguageCode.AN, LanguageCode.AR, LanguageCode.BA, LanguageCode.BE, LanguageCode.BG, LanguageCode.BN, LanguageCode.CA, LanguageCode.CS, LanguageCode.DE, LanguageCode.EL, LanguageCode.EN, LanguageCode.ES, LanguageCode.FR, LanguageCode.GD, LanguageCode.GL, LanguageCode.HI, LanguageCode.HY, LanguageCode.JV, LanguageCode.KO, LanguageCode.MR, LanguageCode.MS, LanguageCode.NO, LanguageCode.PA, LanguageCode.PL, LanguageCode.PS, LanguageCode.PT, LanguageCode.QU, LanguageCode.RM, LanguageCode.RN, LanguageCode.RO, LanguageCode.RU, LanguageCode.SQ, LanguageCode.SR, LanguageCode.SV, LanguageCode.TA, LanguageCode.TH, LanguageCode.TR, LanguageCode.UG, LanguageCode.UK, LanguageCode.UR, LanguageCode.UZ, LanguageCode.VE, LanguageCode.VI, LanguageCode.VO, LanguageCode.WA, LanguageCode.WO, LanguageCode.XH, LanguageCode.YI, LanguageCode.YO, LanguageCode.ZA, LanguageCode.ZH, LanguageCode.ZU ]
             |> List.map
                 (\languageCode ->
