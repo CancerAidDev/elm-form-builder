8eb9bc028fe5decd07d38897a1967d6670c03b2d
Andrew Newman <andrewfnewman@gmail.com>
2022-06-06 15:56:53 +1000

    Test setting fields.

---
+++ tests/Form/FieldSpec.elm
@@ -0,0 +1,38 @@
+module Form.FieldSpec exposing (..)
+
+import Expect
+import Form.Field as Field
+import Form.Field.Direction as Direction
+import Form.Field.Width as Width
+import Fuzz
+import Test
+
+
+suite : Test.Test
+suite =
+    Test.describe "Form.updateStringValue"
+        [ Test.fuzz2 Fuzz.string Fuzz.string "Update StringField " <|
+            \initialString updateString ->
+                let
+                    fieldValues =
+                        { required = False, label = "", width = Width.HalfSize, enabledBy = Nothing, order = 0, value = initialString }
+
+                    field =
+                        Field.text fieldValues
+                in
+                Field.updateStringValue updateString field
+                    |> Expect.equal
+                        (Field.text { fieldValues | value = updateString })
+        , Test.fuzz2 Fuzz.string Fuzz.string "Update Radio " <|
+            \option1 option2 ->
+                let
+                    radioFieldValues =
+                        { required = False, label = "", width = Width.HalfSize, enabledBy = Nothing, order = 0, value = option1, default = Nothing, direction = Direction.Row, options = [ { label = Nothing, value = option1 }, { label = Nothing, value = option2 } ] }
+
+                    field =
+                        Field.radio radioFieldValues
+                in
+                Field.updateStringValue option2 field
+                    |> Expect.equal
+                        (Field.radio { radioFieldValues | value = option2 })
+        ]
